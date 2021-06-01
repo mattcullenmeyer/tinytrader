f4d391f754ca3c8a57bf7b54de2cf080369cdf29
“mattcullenmeyer” <mattcullenmeyer@gmail.com>
2021-06-01 14:10:53 -0600

    fixes for dropdown ui

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Ticker(models.Model):
 	ticker = models.CharField(max_length=6, unique=True)
-	company_name_ticker = models.CharField(max_length=200, blank=True, null=True)
+	company_name_ticker = models.CharField(max_length=200, blank=True, null=True, default='')
 
 	def __str__(self):
 		return f'{self.id} {self.company_name_ticker}'
