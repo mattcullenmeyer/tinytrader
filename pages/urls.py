49630d56a07d0a951d3608a8549386495ff541b4
mattcullenmeyer <mattcullenmeyer@gmail.com>
2021-07-25 13:14:05 -0600

    Added about page and hero image to home

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from pages.views import HomeView, StockView
+from pages.views import HomeView, AboutView, StockView
 
 app_name = 'pages'
 urlpatterns = [
   path('', HomeView.as_view(), name='home'),
+  path('about/', AboutView.as_view(), name='about'),
 
   path('stock/<ticker>/', StockView.as_view(), name='stock'),
 ]
