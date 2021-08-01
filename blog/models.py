ca22d13b5bb439985802f375c73fd0c76ff4b94c
mattcullenmeyer <mattcullenmeyer@gmail.com>
2021-08-01 13:44:14 -0600

    Updated blog model return for admin

--- blog/models.py
+++ blog/models.py
@@ -28,4 +28,4 @@ class Image(models.Model):
   post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
   def __str__(self):
-    return f'{self.name} >> {self.post.title}'
+    return f'{self.name} >> {self.post.slug}'
