d0e9aae8605da445141366c6c7e252733a1f4a0a
mattcullenmeyer <mattcullenmeyer@gmail.com>
2021-12-07 21:06:50 -0700

    Removed mandatory email verification

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,7 @@ app_name = 'users'
 urlpatterns = [
   path('resend-verfication/', login_required(views.resend_verfication), name="resend_verfication"),
   path('dj-rest-auth/', include('dj_rest_auth.urls')),
-  path('dj-rest-auth/registration/account-confirm-email/<str:key>/', ConfirmEmailView.as_view()), # Needs to be defined before registration path
+  path('dj-rest-auth/registration/account-confirm-email/<str:key>/', ConfirmEmailView.as_view()), # needs to be defined before registration path
   path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
   path('dj-rest-auth/account-confirm-email/', VerifyEmailView.as_view(), name='account_email_verification_sent'),
 ]
