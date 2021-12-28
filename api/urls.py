6e0eb0e919001cbad2359ec663a8684763bcae69
mattcullenmeyer <mattcullenmeyer@gmail.com>
2021-12-28 14:33:28 -0700

    Updated endpoints

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
+from dj_rest_auth.views import LoginView, LogoutView
+from dj_rest_auth.registration.views import VerifyEmailView, ConfirmEmailView, RegisterView
 
 from api import views as api_views
 from users import views as user_views
@@ -19,8 +21,14 @@ router.register('users', user_views.UsersViewSet, 'users')
 app_name = 'api'
 urlpatterns = [
   path('', include(router.urls)),
-  path('api-token-auth/', obtain_auth_token, name='api-token-auth'),
+  path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
   path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-  path('signup/email/<str:email>/', user_views.SignupEmailView.as_view(), name='signup-email'),
-  path('user/detail/', user_views.UserDetailView.as_view(), name='user-detail'),
+  path('signup/email/<str:email>/', user_views.SignupEmailView.as_view(), name='signup_email'),
+  path('user/detail/', user_views.UserDetailView.as_view(), name='user_detail'),
+
+  path('login/', LoginView.as_view(), name='rest_login'),
+  path('logout/', LogoutView.as_view(), name='rest_logout'),
+  path('account-confirm-email/<str:key>/', ConfirmEmailView.as_view()), # needs to be defined before registration path
+  path('signup/', RegisterView.as_view(), name='rest_register'),
+  path('account-confirm-email/', VerifyEmailView.as_view(), name='account_email_verification_sent'), # only needed if email verification is mandatory
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 from users import views
 from django.contrib.auth.decorators import login_required
-from dj_rest_auth.registration.views import VerifyEmailView, ConfirmEmailView
 
 app_name = 'users'
 urlpatterns = [
   path('resend-verfication/', login_required(views.resend_verfication), name="resend_verfication"),
-  path('dj-rest-auth/', include('dj_rest_auth.urls')),
-  path('dj-rest-auth/registration/account-confirm-email/<str:key>/', ConfirmEmailView.as_view()), # needs to be defined before registration path
-  path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-  path('dj-rest-auth/account-confirm-email/', VerifyEmailView.as_view(), name='account_email_verification_sent'),
 ]
